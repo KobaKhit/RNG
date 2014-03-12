d187b314cf7e0c9493db5d585fd2919e450dbd23
dostre <kobakhit@gmail.com>
2014-03-12 17:21:51 -0400

    Random number generator in C++

--- RNGs.cpp
+++ RNGs.cpp
@@ -1,4 +1,4 @@
-// This program is a random number generator that uses the random number generaton algorithm 
+// This program is a random number generator that uses the random number generation algorithm 
 // "Real Version 2" from the paper "Random Numbers: Good Ones are Hard to Find" by Park and Miller.
 // The "Real Version 2" algorithm in the paper on page 3 is presented in Pascal code. In essence this 
 // program is the C++ version of "Real Version 2" algorithm written originally in Pascal.
